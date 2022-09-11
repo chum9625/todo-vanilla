73e06818550424a91f5be0d882e007cce261fed4
masverse <17173386+chum9625@users.noreply.github.com>
2022-09-11 23:02:18 +0900

    added js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,74 @@
 import "./styles.css";
 
 const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
-  alert(inputText);
+  document.getElementById("add-text").value = "";
+
+  // li生成
+  const li = document.createElement("li");
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // p生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  //button(完了)生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // 完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+
+    // TODO内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+
+    // div以下を初期化
+    addTarget.textContent = null;
+
+    // p生成
+    const p = document.createElement("p");
+    p.innerText = text;
+
+    // button生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    // liタグ子要素に各要素を設定
+    addTarget.appendChild(p);
+    addTarget.appendChild(backButton);
+
+    // 押された完了ボタンの親タグliタグを未完了リストから削除
+    deleteFromIncompleteList(div.parentNode);
+
+    // 完了リストに追加
+    document.getElementById("complete-list").appendChild(div.parentNode);
+  });
+
+  //button(削除)生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 削除ボタンのliタグを未完了リストから削除
+    deleteFromIncompleteList(div.parentNode);
+  });
+
+  // liタグの子要素に各要素を設定
+  li.appendChild(div);
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
