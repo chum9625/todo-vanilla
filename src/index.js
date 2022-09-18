04429e385e9db14f6d5e3a39ff6ef08badf38951
masverse <17173386+chum9625@users.noreply.github.com>
2022-09-18 22:19:11 +0900

    関数まとめ、htmlの不要タグ削除

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  createIncompleteList(inputText);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
   // li生成
   const li = document.createElement("li");
 
@@ -14,7 +24,7 @@ const onClickAdd = () => {
 
   // p生成
   const p = document.createElement("p");
-  p.innerText = inputText;
+  p.innerText = text;
 
   //button(完了)生成
   const completeButton = document.createElement("button");
@@ -37,7 +47,13 @@ const onClickAdd = () => {
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
     backButton.addEventListener("click", () => {
-      alert();
+      // 押された戻すボタンの親タグ(li)を完了リストから削除
+      const deleteTarget = addTarget.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      // テキスト取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
     });
 
     // liタグ配下のHTML階層設定
@@ -69,11 +85,10 @@ const onClickAdd = () => {
   document.getElementById("incomplete-list").appendChild(li);
 };
 
-// 未完了リストから指定の要素を削除
-const deleteFromIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
-};
-
 document
   .getElementById("add-button")
   .addEventListener("click", () => onClickAdd());
+
+/** 関数化のコツ
+ * 共通化できる部分は関数化して、条件が異なる部分に引数を渡す
+ */
